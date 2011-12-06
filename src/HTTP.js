878338c981d5bf4caaad9225665473d7de4f5349
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2011-12-06 13:38:29 +0200

    Added cbNetwork.js and Server.js

--- src/HTTP.js
+++ src/HTTP.js
@@ -1,1 +1,5 @@
 // HTTP Methods like Get, DownloadState, SaveContent, LoadContent (Well probably different for Node, because just abstracts from the Node API)
+
+cbNetwork.HTTP.Get = function () {
+
+};
---
+++ src/cbNetwork.js
@@ -0,0 +1,3 @@
+var cbNetwork = {};
+
+exports.cbNetwork = cbNetwork;
