9a68a37e89296d9c79fcbddf77ce4a063bc7c3f7
Vesa Laakso <laakso.vesa@gmail.com>
2011-12-23 15:10:49 +0200

    Built the documentation

--- src/Logger.js
+++ src/Logger.js
@@ -9,48 +9,95 @@ var colors = require('colors');
  * @class Creates a new logger with the desired prefix and timestamp
  * @requires colors
  *
- * @param {String} [prefix=[Logger] (HH:MM:SS)]   Message to apply to the start of the message %t will be replaced with a timestamp.
+ * @param {String} [prefix]   Message to apply to the start of the message %t will be replaced with a timestamp.
  *                                                For example: '[My logger :) [%t]]' Will print [My logger :) [00:42:30]]
  */
 function Logger(prefix) {
-  this.prefix = prefix || '[Logger]'.cyan + '(%t)';
+  this.prefix = prefix || '';
 }
 
 /**
  * @param {String} msg  Message to print
  */
-Logger.prototype.write = function (msg) {
-  var s = '', d = new Date();
+Logger.prototype.write = function () {
+  var prfx = ''
+    , d = new Date()
+    , msg = arguments[0]
+    , args;
+  // Create prefix
   if (this.prefix) {
-    s = this.prefix
+    prfx = this.prefix
     // Apply timestamp
       .replace('%t',
         p(d.getHours())   + ':' +
         p(d.getMinutes()) + ':' +
         p(d.getSeconds()));
   }
-  console.log(s + msg);
+  // Handle vars in message
+  if (arguments.length > 1) {
+    // convert it into a real array
+    args = Array.prototype.slice.call(arguments, 1);
+    for (var i = args.length; i--;) {
+      msg = msg.replace(new RegExp('%' + i, 'g'), args[i]);
+    }
+  }
+  
+  console.log(prfx + msg);
 };
 
 /**
- * @param {String} msg  Message to print green
+ * @param {String} msg     Message to print with a green [INFO]-tag
+ * @param {String} [var0]  Variable to replace %0 from msg
+ * @param {String} [var1]  Variable to replace %1 from msg
+ * @param {String} [varN]  Variable to replace %n from msg and so forth
  */
-Logger.prototype.info = function (msg) {
-  this.write(msg.green);
+Logger.prototype.info = function () {
+  arguments[0] = '[INFO] '.green + arguments[0];
+  this.write.apply(this, arguments);
 };
 
 /**
- * @param {String} msg  Message to print yellow
+ * @param {String} msg  Message to print with a yellow [WARN]-tag
+ * @param {String} [var0]  Variable to replace %0 from msg
+ * @param {String} [var1]  Variable to replace %1 from msg
+ * @param {String} [varN]  Variable to replace %n from msg and so forth
  */
-Logger.prototype.warn = Logger.prototype.notice = function (msg) {
-  this.write(msg.yellow);
+Logger.prototype.warn = function () {
+  arguments[0] = '[WARN] '.yellow + arguments[0];
+  this.write.apply(this, arguments);
 };
 
 /**
- * @param {String} msg  Message to print red and bold
+ * @param {String} msg  Message to print with a yellow [NOTICE]-tag
+ * @param {String} [var0]  Variable to replace %0 from msg
+ * @param {String} [var1]  Variable to replace %1 from msg
+ * @param {String} [varN]  Variable to replace %n from msg and so forth
  */
-Logger.prototype.error = Logger.prototype.fatal = function (msg) {
-  this.write(msg.red.bold);
+Logger.prototype.notice = function () {
+  arguments[0] = '[NOTICE] '.yellow + arguments[0];
+  this.write.apply(this, arguments);
+};
+
+/**
+ * @param {String} msg  Message to print with a red & bold [ERROR]-tag
+ * @param {String} [var0]  Variable to replace %0 from msg
+ * @param {String} [var1]  Variable to replace %1 from msg
+ * @param {String} [varN]  Variable to replace %n from msg and so forth
+ */
+Logger.prototype.error = function () {
+  arguments[0] = '[ERROR] '.red.bold + arguments[0];
+  this.write.apply(this, arguments);
+};
+
+/**
+ * @param {String} msg  Message to print with a red & bold [FATAL]-tag
+ * @param {String} [var0]  Variable to replace %0 from msg
+ * @param {String} [var1]  Variable to replace %1 from msg
+ * @param {String} [varN]  Variable to replace %n from msg and so forth
+ */
+Logger.prototype.fatal = function () {
+  arguments[0] = '[FATAL] '.red.bold + arguments[0];
+  this.write.apply(this, arguments);
 };
 
 /** @ignore */
